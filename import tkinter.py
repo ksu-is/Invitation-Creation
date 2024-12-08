4b973c3715d911c8acd4b2e04483b5be4ecc4663
ramoni-crowe <ocrowe1@students.kennesaw.edu>
2024-12-08 18:24:53 -0500

    tessting

--- import tkinter.py
+++ import tkinter.py
@@ -1,4 +1,6 @@
+import tkinter
 from tkinter import *
+
 match_color = input("What color: ")
 
 class Calculator(Frame):
@@ -12,6 +14,21 @@ class Calculator(Frame):
         self.create_widgets()
         self.bind_buttons(master)
         self.grid()
+        
+    def add_chr(self, char, btn=None):
+        """
+        Concatenates a character passed from a button press (or key type) 
+        to a string.
+        :param char: string to add passed from a button
+        :param btn: button name to use if key is pressed (to flash)
+        :return: None
+        """
+        self.entry.configure(state="normal")
+        self.flash(btn) # Flash a button correspond to keystroke
+        if self.entry.get() == "Invalid Input":
+            self.entry.delete(0,END)
+        self.entry.insert(END, char)
+        self.entry.configure(state="disabled")
 
 root = Tk()
 root.geometry()
