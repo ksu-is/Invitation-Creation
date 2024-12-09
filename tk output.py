4c91f8738a376201b669eed6c85bcc4e35e41631
ramoni-crowe <ocrowe1@students.kennesaw.edu>
2024-12-08 19:37:16 -0500

    Update tk output.py

--- tk output.py
+++ tk output.py
@@ -4,8 +4,8 @@ from tkinter import messagebox
 def invitation():
     bg_color = input("What color do you want on the background: ")
     
-root = tk.Tk()
-root.title("Your letter: ")
+    root = tk.Tk()
+    root.title("Your letter: ")
 
 #color_input = tk.Entry(root, width=20)
 
