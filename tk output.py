595db87e31350d988af6d4d9ca2bdce75491f8db
ramoni-crowe <ocrowe1@students.kennesaw.edu>
2024-12-08 20:25:05 -0500

    Update tk output.py

--- tk output.py
+++ tk output.py
@@ -3,22 +3,30 @@ from tkinter import messagebox
 
 def invitation():
     bg_color = input("What color do you want on the background: ")
-    name = input("What is your name?")
+    name = input("What is your name?: ")
     #root is tk window
     root = tk.Tk() #initializes the window 
-    root.title("This invitation is from" + name)
+    root.title("This invitation is from, " + name)
     #50x50 ellipsis 5000x5000 full screen 
     root.geometry("700x500")
     root.configure(bg=bg_color)
+    
+    #info for inv 
+    title_iv = input("Please enter the title of your invitation: ")
+    iv_to = input("Please enter the name of the recipient: ")
+    body = input("Please enter the important text for your recipient: ")
    
     #put ontop of the window
-    Label = tk.Label(root, text=name, font=("Helvetica", 18), bg=bg_color, fg="black")
+    Label = tk.Label(root, text=title_iv, font=("Helvetica", 18), bg=bg_color, fg="black")
     Label.grid(row=0, column=1, pady=10, padx=10) #pady/x is padding 
     
-    #The grid 
+    Label = tk.Label(root, text=iv_to, font=("Helvetica", 18), bg=bg_color, fg="black")
+    Label.grid(row=1, column=1, pady=10, padx=10) 
+    
+    '''#The grid 
     root.grid_columnconfigure(0, weight=1) #center the columns 
     root.grid_columnconfigure(1, weight=1)
-    root.grid_columnconfigure(2, weight=1)
+    root.grid_columnconfigure(2, weight=1)'''
     
     #do i need?
     root.mainloop()
