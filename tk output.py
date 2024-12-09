4ea2bb65f8d3ffc73fd04ca42e153bffa83cd0f7
ramoni-crowe <ocrowe1@students.kennesaw.edu>
2024-12-08 20:02:25 -0500

    Update tk output.py

--- tk output.py
+++ tk output.py
@@ -3,14 +3,17 @@ from tkinter import messagebox
 
 def invitation():
     bg_color = input("What color do you want on the background: ")
+    name = input("What is your name?")
     #root is tk window
     root = tk.Tk() #initializes the window 
     root.title("Your letter: ")
     #50x50 ellipsis 5000x5000 full screen 
     root.geometry("700x500")
     root.configure(bg=bg_color)
-
-#color_input = tk.Entry(root, width=20)
+    #do i need?
     root.mainloop()
+    #put ontop of the window
+    labe1 = tk.labe1(root, text=name, font=("Helvetica", 18), bg=bg_color)
+    labe1.pack(side="top", pady=10)
     
 invitation()
