d2483fa1714510100b80568bc53ae4bbd2f1b425
ramoni-crowe <ocrowe1@students.kennesaw.edu>
2024-12-08 20:48:39 -0500

    Update Invitation_Creation.py

--- Invitation_Creation.py
+++ Invitation_Creation.py
@@ -17,22 +17,32 @@ def invitation():
     body = input("Please enter the important text for your recipient: ")
    
     #put ontop of the window
-    Label = tk.Label(root, text=title_iv, font=("Helvetica", 18), bg=bg_color, fg="black")
-    Label.grid(row=0, column=1, pady=10, padx=10) #pady/x is padding 
+    Label = tk.Label(root, text=title_iv, font=("Helvetica", 20), bg=bg_color, fg="black")
+    Label.grid(row=0, column=3, pady=10, padx=10) #pady/x is padding 
     
     Label = tk.Label(root, text="To: "+ iv_to, font=("Helvetica", 18), bg=bg_color, fg="black")
-    Label.grid(row=1, column=1, pady=10, padx=10) 
+    Label.grid(row=6, column=6, pady=10, padx=10) 
     
     Label = tk.Label(root, text=body, font=("Helvetica", 18), bg=bg_color, fg="black")
-    Label.grid(row=2, column=2, pady=10, padx=10) 
+    Label.grid(row=1, column=1, pady=10, padx=10) 
     
     #The grid 
     root.grid_columnconfigure(0, weight=1) #columns 
     root.grid_columnconfigure(1, weight=1)
     root.grid_columnconfigure(2, weight=1)
+    root.grid_columnconfigure(3, weight=1)  
+    root.grid_columnconfigure(4, weight=1)
+    root.grid_columnconfigure(5, weight=1)
+    root.grid_columnconfigure(6, weight=1)
     root.grid_rowconfigure(0, weight=1) #rows 
     root.grid_rowconfigure(1, weight=1)
     root.grid_rowconfigure(2, weight=1)
+    root.grid_rowconfigure(3, weight=1)
+    root.grid_rowconfigure(4, weight=1)
+    root.grid_rowconfigure(5, weight=1)
+    root.grid_rowconfigure(6, weight=1)
+    root.grid_rowconfigure(7, weight=1)
+
     
     #do i need?
     root.mainloop()
